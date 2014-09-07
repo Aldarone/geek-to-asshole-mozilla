d639a19a5e31c61e67d0d47803e923b32cd7f9bc
Alda Marteau-Hardi <alda@leetchee.fr>
2014-09-07 09:36:59 +0200

    First version of GeekToAsshole

--- readme.md
+++ readme.md
@@ -1,11 +1,14 @@
-# cloud-to-butt-mozilla
+# geek-to-asshole-mozilla
 
-Derived from the work of Steven Frank's [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) Chrome extension.
+Derived from the work of Steven Frank's [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) Chrome extension and Chris Wright's [cloud-to-butt-mozilla](https://github.com/DaveRandom/cloud-to-butt-mozilla) Firefox extension.
 
+It replaces the words 'Geek' and 'Gamer' with 'Asshole' because after the GamerGate I think it suits them better.
+
+To know more about the situation you can read Gamasutra's [Gamers are over](http://www.gamasutra.com/view/news/224400/Gamers_dont_have_to_be_your_audience_Gamers_are_over.php) or BadassDigest's [Guide to Assholes](http://badassdigest.com/2014/08/26/video-games-misogyny-and-terrorism-a-guide-to-assholes/).
 
 ## Installing
 
-Download [CloudToButt.xpi](https://github.com/DaveRandom/cloud-to-butt-mozilla/blob/master/CloudToButt.xpi?raw=true)
+Download [GeekToAsshole.xpi](https://github.com/Aldarone/geek-to-asshole-mozilla/blob/master/GeekToAsshole.xpi?raw=true)
 
 
 ## Building
---
+++ src/chrome/content/geek-to-asshole.js
@@ -0,0 +1,58 @@
+(function() {
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+            if(node.parentElement.tagName.toLowerCase() != "script") {
+                handleText(node);
+            }
+			break;
+	}
+}
+
+function handleText(textNode) {
+	var v = textNode.nodeValue;
+
+  // Deal with geeks
+  v = v.replace(/\b(G|g)(eek|amer)(s)?/g, function(match, p1, p2, p3, offset, string) {
+    // g - 6 = a
+    // s is to deal with plural
+    a = String.fromCharCode(p1.charCodeAt(0) - 6);
+    s = p3;
+    return a + 'sshole' + s;
+  });
+
+	textNode.nodeValue = v;
+}
+
+    function windowLoadHandler()
+    {
+        // Dear Mozilla: I hate you for making me do this.
+        window.removeEventListener('load', windowLoadHandler);
+
+        document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(e) {
+            walk(e.originalTarget.body);
+        });
+    }
+
+    window.addEventListener('load', windowLoadHandler);
+}());
